e03b7f08a2fb8a7e077b941b8a3360ee866a9c9c
Justina <73461989+talalaite@users.noreply.github.com>
2021-01-19 15:41:42 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 //Movement Animation to happen
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
+
 //Items
 const title = document.querySelector(".title");
 const logo = document.querySelector(".logo img");
-const contacts = document.querySelector(".contacts");
 const description = document.querySelector(".info h3");
-const nav = document.querySelector("nav");
+const portfolio = document.querySelector(".portfolio a");
+const cv = document.querySelector(".cv a");
+const contacts = document.querySelector(".contacts a");
 
 //Moving Animation Event
 container.addEventListener("mousemove", (e) => {
@@ -14,24 +16,30 @@ container.addEventListener("mousemove", (e) => {
   let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
+
 //Animate In
 container.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
+  card.style.transition = "all 0.2s ease";
+
   //Popout
   title.style.transform = "translateZ(150px)";
   logo.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
-  nav.style.transform = "translateZ(100px)";
-  contacts.style.transform = "translateZ(75px)";
+  portfolio.style.transform = "translateZ(100px)";
+  cv.style.transform = "translateZ(100px)";
+  contacts.style.transform = "translateZ(100px)";
 });
+
 //Animate Out
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
   //Popback
   title.style.transform = "translateZ(0px)";
   logo.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
-  nav.style.transform = "translateZ(0px)";
+  portfolio.style.transform = "translateZ(0px)";
+  cv.style.transform = "translateZ(0px)";
   contacts.style.transform = "translateZ(0px)";
 });
