d85aad0235b42c05af93fe21edc35a49a9842d36
Justina <73461989+talalaite@users.noreply.github.com>
2021-01-21 19:51:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-//
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
-//Items
 const title = document.querySelector(".title");
 const logo = document.querySelector(".logo img");
 const description = document.querySelector(".info h3");
@@ -10,18 +8,15 @@ const portfolio = document.querySelector(".portfolio a");
 const cv = document.querySelector(".cv a");
 const contacts = document.querySelector(".contacts a");
 
-//Moving Animation Event
 container.addEventListener("mousemove", (e) => {
   const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
   const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-  card.style.transform = `rotateY(${xAxis - 1}deg) rotateX(${yAxis - 1}deg)`;
+  card.style.transform = `rotateY(${xAxis * -1}deg) rotateX(${yAxis}deg)`;
 });
 
-//Animate In
 container.addEventListener("mouseenter", (e) => {
   card.style.transition = "all 0.2s ease";
 
-  //Popout
   title.style.transform = "translateZ(150px)";
   logo.style.transform = "translateZ(200px) rotateZ(-45deg)";
   description.style.transform = "translateZ(125px)";
@@ -30,12 +25,10 @@ container.addEventListener("mouseenter", (e) => {
   contacts.style.transform = "translateZ(100px)";
 });
 
-//Animate Out
 container.addEventListener("mouseleave", (e) => {
   card.style.transition = "all 0.5s ease";
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 
-  //Popback
   title.style.transform = "translateZ(0px)";
   logo.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
